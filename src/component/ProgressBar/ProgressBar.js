e96821a35c44dc498eaf9486b16d2c68441dfd51
tsnm-x <tasneemsm1234@gmail.com>
2023-01-03 23:51:32 +0200

    edited progress bar

--- src/component/ProgressBar/ProgressBar.js
+++ src/component/ProgressBar/ProgressBar.js
@@ -34,13 +34,20 @@ class ProgressBar extends PureComponent{
                     return (
                         <>
                             <div className="progress-step">
-                                <div className={`progress-step-num bubble-animation`}>{this.state?.currentIndex > index ? (<div dangerouslySetInnerHTML={{__html: '&check;'}}></div>) : index+1}</div>
+                                <div className={`progress-step-num ${this.state?.currentIndex > index || this.state?.currentIndex === index ? "bubble-animation": ""}`}>
+                                    {index !== (this.props.steps?.length-1)? 
+                                        this.state?.currentIndex > index? 
+                                        (<div dangerouslySetInnerHTML={{__html: '&check;'}}></div>) : index+1
+                                        : this.state?.currentIndex === index? 
+                                        (<div dangerouslySetInnerHTML={{__html: '&check;'}}></div>) : index+1
+                                        }
+                                    </div>
                                 <div>{step?.progressBarTag}</div>
                             </div>
                             {
                                 index !== (this.props.steps?.length-1) ?
                                 <div className="Progress-bar-line-container" style={{width: `${100/(this.props.steps?.length+1)}%`}}>
-                                    <div className={`Progress-bar-line ${ this.state?.currentIndex < index? "" : "animation"}`}></div>
+                                    <div className={`Progress-bar-line ${ this.state?.currentIndex < index+1? "" : "animation"}`}></div>
                                 </div>
                                 : 
                                 <div className="Progress-bar-line-container" style={{width: `${100/this.props.steps?.length}%`}}>
